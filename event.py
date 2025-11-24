476de6cdba0730391cbdf5d24729c5abf3df107a
ZODAGOATFR <lonzojr7@gmail.com>
2025-11-24 13:56:39 -0600

    Create a remove_event method that removes all events in the schedule that have the same what attribute as the given event.

--- event.py
+++ event.py
@@ -9,3 +9,8 @@ class Event:
     def __str__(self):
         formatted_time = self.when.strftime("%m/%d/%Y at %I:%M %p")
         return f"Event: {self.what} at {formatted_time}"
+    # Use Case 1: Remove all events matching this event's "what"
+    def remove_event(self, event_list: list):
+        original_len = len(event_list)
+        event_list[:] = [e for e in event_list if e.what != self.what]
+        return len(event_list) < original_len
