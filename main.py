1a25ea0dd60ba11a8d19d5af076faf52cd01b141
Robin <robinhallct@gmail.com>
2025-11-25 22:34:40 -0600

    Add display message to function

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ def print_welcome_banner():
     print(banner)
 
 def remove_two_sessions(session, profileA, profileB):
+    removed = session.remove(profileA, profileB)
+    if removed:
+        print(f"Removed session on {session.topic} from one or both profiles' schedules.")
+    else:
+        print("Session not found in either profile's schedule.")
 
 def main():
     print (print_welcome_banner())
