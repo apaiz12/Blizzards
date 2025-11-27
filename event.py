95ada728db62905c99af84cd375fcbf571efa4a2
Alina <apaiz@xula.edu>
2025-11-26 23:30:25 -0600

    create add_event method

--- event.py
+++ event.py
@@ -6,6 +6,20 @@ class Event:
         self.what = what
         self.when = when
 
+    def add_event(self, profile):
+        if self in profile.schedule:
+            return False
+        for event in profile.schedule:
+            if event.what == self.what and event.when == self.when:
+                print(f"Cannot add {self.what} for {profile.first_name} {profile.last_name}: Conflict or duplicate event.")
+                return False
+        profile.schedule.append(self)
+        print(f"Added {self.what} to {profile.first_name} {profile.last_name}'s schedule.")
+        return True
+        
+        
+
+
     def __str__(self):
         formatted_time = self.when.strftime("%m/%d/%Y at %I:%M %p")
         return f"Event: {self.what} at {formatted_time}"
